92b1b85b385031b3fc4d68fd02371f096bb97f03
Mykhailo Koreshkov <mk43@sanger.ac.uk>
2023-12-07 03:47:54 +0000

    fix shape issue in worker

--- src/fastlbp_imbg/fastlbp.py
+++ src/fastlbp_imbg/fastlbp.py
@@ -35,7 +35,7 @@ def __worker_skimage(args):
         output_offset = job['output_offset']
         
         patchsize = job['patchsize']
-        h,w,nchannels = shape
+        nchannels,h,w = shape
         nprows, npcols = h//patchsize, w//patchsize
         
         job_nfeatures = job['npoints']+2
@@ -74,13 +74,14 @@ def __worker_skimage(args):
 
             img_data_shm = shared_memory.SharedMemory(name=job['img_shm_name'])
             img_data = np.ndarray(shape, dtype=job['img_pixel_dtype'], buffer=img_data_shm.buf)
-            log.info(f"run_skimage: worker {jobname}({pid}): img_data flags are: {str(img_data.flags)}")
             
-            img_channel_data = img_data[job['channel']]
-            log.info(f"run_skimage: worker {jobname}({pid}): img_channel_data flags are: {str(img_channel_data.flags)}")
+            img_channel = img_data[job['channel']]
+            log.info(f"run_skimage: worker {jobname}({pid}): contiguity test: img_mask {img_mask.flags.c_contiguous}, img_data {img_data.flags.c_contiguous}, img_channel {img_channel.flags.c_contiguous}")
+            assert img_channel.shape == img_mask.shape, \
+                f"shape mismatch. something is horrendously wrong. img_channel({img_channel.shape})!=img_mask({img_mask.shape})"
             
             lbp_results = uniform_local_binary_pattern(
-                image=img_channel_data, P=job['npoints'], R=job['radius']
+                image=img_channel, P=job['npoints'], R=job['radius']
             ) #.astype(np.uint16)
             
             # log.info(f"run_skimage: worker {jobname}({pid}): image: min={img_data[:,:,job['channel']].min()} max={img_data[:,:,job['channel']].max()} avg={img_data[:,:,job['channel']].mean()}")
