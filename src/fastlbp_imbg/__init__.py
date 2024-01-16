5abbd2a9fe17eeb3d8e398d019ea56eb2356323a
mkrooted256 <koreshov.m@gmail.com>
2024-01-16 16:13:21 +0200

    a little more refactoring

--- src/fastlbp_imbg/__init__.py
+++ src/fastlbp_imbg/__init__.py
@@ -14,4 +14,4 @@ from .fastlbp import (
 __all__ = ["run_fastlbp", "get_radii", "get_p_for_r", "load_sample_image", "create_sample_image", "lbp", "_lbp"]
 
 # I will use the following versioning scheme https://stackoverflow.com/a/76129798
-__version__ = "0.0.2"  
+__version__ = "0.0.2.dev0+refactoring"  
