d773e4c610d1cffc329208e9b8d8c86e48f0c5e8
Dmytro Horyslavets <nepotlet@gmail.com>
2025-07-18 18:15:25 +0300

    upd version to 0.2.3.dev0

--- src/fastlbp_imbg/__init__.py
+++ src/fastlbp_imbg/__init__.py
@@ -36,4 +36,4 @@ __all__ = [
 # - main branch gets 1.2.3
 # - dev branch gets 1.2.3.dev1
 # - feature branch gets 1.2.3.dev0+feature.improve.logs
-__version__ = "0.2.1.dev0+test.test.pypi.publish.github.worklfow"  
+__version__ = "0.2.3.dev0"  
