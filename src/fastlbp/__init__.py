407336cbc6aae7811b0985259754529ea08e2439
Dmytro Horyslavets <nepotlet@gmail.com>
2025-10-15 13:11:55 +0300

    Bump version: 0.2.5.dev0 → 0.2.6.dev0

--- src/fastlbp/__init__.py
+++ src/fastlbp/__init__.py
@@ -38,4 +38,4 @@ __all__ = [
 # - main branch gets 1.2.3
 # - dev branch gets 1.2.3.dev1
 # - feature branch gets 1.2.3.dev0+feature.improve.logs
-__version__ = "0.2.5.dev0"  
+__version__ = "0.2.6.dev0"  
